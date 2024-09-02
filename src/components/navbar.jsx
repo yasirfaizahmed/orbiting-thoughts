c3a77c62f9be3b217a32afdd04e0c0e1c76c782a
xd <yasirfaizahmed.n@gmail.com>
2024-09-02 22:27:14 +0530

    added signin modal

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,13 +5,22 @@ function Navbar() {
   // navbar dropdown for smaller screens
   const [isDropdownVisible, setDropdownVisible] = useState(false);
 
-  // modal handler
-  const [isModalVisible, setModalVisible] = useState(false);
-  const openModal = () => {
-    setModalVisible(true);
+  // signup modal handler
+  const [isSignupModalVisible, setSignupModalVisible] = useState(false);
+  const openSignupModal = () => {
+    setSignupModalVisible(true);
   }
-  const closeModal = () => {
-    setModalVisible(false);
+  const closeSignupModal = () => {
+    setSignupModalVisible(false);
+  }
+
+  //signin modal handler
+  const [isSigninModalVisible, setSigninModalVisible] = useState(false);
+  const openSigninModal = () => {
+    setSigninModalVisible(true);
+  }
+  const closeSigninModal = () => {
+    setSigninModalVisible(false);
   }
 
   return (
@@ -28,8 +37,8 @@ function Navbar() {
           <a className='hoverable' href="#">Contact</a>
       </div>
       <div className="nav-buttons">
-          <button className="login">Sign in</button>
-          <button className="signup" onClick={openModal}>Sign up</button>
+          <button className="login" onClick={openSigninModal}>Sign in</button>
+          <button className="signup" onClick={openSignupModal}>Sign up</button>
       </div>
       <div className="hamburger" onClick={() => setDropdownVisible(!isDropdownVisible)}>
           <span></span>
@@ -42,22 +51,37 @@ function Navbar() {
           <a className='hoverable' href="#">Services</a>
           <a className='hoverable' href="#">Contact</a>
           <div className="dropdown-buttons">
-            <button className="login" >Sign in</button>
-            <button className="signup" onClick={openModal}>Sign up</button>
+            <button className="login" onClick={openSigninModal}>Sign in</button>
+            <button className="signup" onClick={openSignupModal}>Sign up</button>
           </div>
           
       </div>
     </nav>
 
-    {isModalVisible && (
+    {isSignupModalVisible && (
       <div className="signup-modal">
           <div className="modal-content">
-              <span className="close" onClick={closeModal}>&times;</span>
+              <span className="close" onClick={closeSignupModal}>&times;</span>
               <h2>Sign Up</h2>
               <form className='signupForm'>
                 <input type="email" id="email" name="email" required placeholder="Email"/>
+                <input type="username" id="username" name="username" required placeholder="User name"/>
                 <input type="password" id="password" name="password" required placeholder="Password"/>
                 <button className="signupSubmitButton" type="submit">Sign Up</button>
+              </form>
+          </div>
+      </div>
+    )}
+
+    {isSigninModalVisible && (
+      <div className="signup-modal">
+          <div className="modal-content">
+              <span className="close" onClick={closeSigninModal}>&times;</span>
+              <h2>Sign In</h2>
+              <form className='signupForm'>
+                <input type="email" id="email" name="email" required placeholder="Email"/>
+                <input type="password" id="password" name="password" required placeholder="Password"/>
+                <button className="signupSubmitButton" type="submit">Sign In</button>
               </form>
           </div>
       </div>
